c2556bd9ac31d02b0ae03842e0877e757c65e9e2
Reet Kapoor <reetkapoor2915@gmail.com>
2025-07-31 14:28:20 +0530

    changes

--- src/Healthcare/Multilingual_Symptom_Checker/chatbot_utils.py
+++ src/Healthcare/Multilingual_Symptom_Checker/chatbot_utils.py
@@ -18,8 +18,7 @@ def get_response_from_gemini(user_message, model):
     if model is None:
         return "I'm sorry, I can't connect right now. Please check the API configuration."
     else:
-        return "
-        Connecting to the model, please wait..."
+        return "Connecting to the model, please wait..."
 
     prompt = f"""
     You are a helpful healthcare assistant. Based on the symptoms, suggest possible illness in a practical yet not scary way. Also, mention the urgency level (mild , moderate, severe). If the symptoms are not related to any illness, suggest that the user consult a doctor for further evaluation.
